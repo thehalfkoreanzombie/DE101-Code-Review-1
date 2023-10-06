d02c54e96d4176f27bc2ea9ddb91fd359ae269b8
Alex Wallace <kairo@Alexs-MacBook-Pro.local>
2023-10-06 12:18:11 -0700

    extra comments

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ b = 9.12345678 #assigned a float
 c = 'this is a string' #assigned a string
 grab_bag = [a, b, c] #assigned variables before list for convenience 
 print(grab_bag) #Printed the grab_bag just to make sure list is legible 
-puppies = {'Max': 6, 'Apollo': 15, 'Bubbles': 3}
-print(puppies)
-print(puppies.keys())
+puppies = {'Max': 6, 'Apollo': 15, 'Bubbles': 3} #list of 3 keys and values
+print(puppies) #just to make sure the list works
+print(puppies.keys()) #syntax to print only the keys of the dictionary
