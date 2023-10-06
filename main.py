d01881ad1a5d4cb38f569aabea5e9a160ebdc20c
Alex Wallace <kairo@Alexs-MacBook-Pro.local>
2023-10-06 12:05:02 -0700

    Adding the puppies dictionary

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ b = 9.12345678 #assigned a float
 c = 'this is a string' #assigned a string
 grab_bag = [a, b, c] #assigned variables before list for convenience 
 print(grab_bag) #Printed the grab_bag just to make sure list is legible 
+puppies = {'Max': 6, 'Apollo': 15, 'Bubbles': 3}
+print(puppies)
+print(puppies.keys())
